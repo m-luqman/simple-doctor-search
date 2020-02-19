628c20a3c6331c844468c5948774e0dc4b95ea27
muhammadluqman <muhammadluqman@172.16.9.156>
2020-02-19 07:00:56 +0530

    made availability field more realistic

--- src/main/resources/doctor_index_generator.py
+++ src/main/resources/doctor_index_generator.py
@@ -1,15 +1,18 @@
 import names
 import random
 import json
+from datetime import date
+from _datetime import timedelta
+from email.policy import default
 
 doctor_index=[]
-
+today=date.today()
 for id in range(1000):
     doctor_id={"index":{"_id":id}}
     doctor_data={}
     doctor_data["gender"]=random.choice(["male","female"])
     doctor_data["name"]=names.get_first_name(doctor_data["gender"])
-    doctor_data["availability"]=random.choice(["always","today","next 3 days","coming weekend"])
+    doctor_data["availability"]=list(set(today+timedelta(days=day) for day in range(random.randint(1,15))))
     doctor_data["experience"]=random.randint(0,50)
     doctor_data["rating"]=random.randint(1,5)
     doctor_data["relevance"]=random.random()
@@ -21,4 +24,4 @@ for id in range(1000):
 
 with open("doctor_index.json","w") as file:
     for obj in doctor_index:
-        file.write(json.dumps(obj)+"\n")
+        file.write(json.dumps(obj,default=str)+"\n")
